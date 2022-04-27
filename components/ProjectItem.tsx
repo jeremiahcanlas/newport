84ee8e4795f4090919437f02cc1ff8683a100490
jeremiahcanlas <jeremiahcanlas@gmail.com>
2022-04-26 23:47:36 -0400

    added react-tilt to project image

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -9,17 +9,20 @@ import {
   Text,
 } from "@chakra-ui/react";
 import Image from "next/image";
+import Tilt from "react-tilt";
 
 // WORK on this one !!
 const ProjectItem = ({ project }: any) => {
   return (
-    <Stack direction={"column"}>
-      <Container borderRadius="2px" padding="0">
+    <Stack direction={["column", "column", "row"]} spacing="6">
+      <Container minW={["50%"]} borderRadius="2px" padding="0">
         <Box>
           <Text fontWeight={"700"} letterSpacing="1px" fontSize="1.2em">
             {project.name}
           </Text>
-          <Text lineHeight={"7"}>{project.info1}</Text>
+          <Text lineHeight={"7"} letterSpacing="0.2px" mt="1em">
+            {project.info1}
+          </Text>
         </Box>
 
         <Box mt="1em">
@@ -42,18 +45,42 @@ const ProjectItem = ({ project }: any) => {
           </Button>
         </Stack>
       </Container>
-      <Box display="block">
-        <Image
-          src={project.image}
-          alt="Picture of the author"
-          width="400px"
-          height="300px"
-          layout="responsive"
-          quality={40}
-        />
-      </Box>
+      <Container margin="0" p={0}>
+        <Tilt
+          options={{
+            reverse: false,
+            max: 5,
+            perspective: 1000,
+            scale: 1,
+            speed: 500,
+            transition: true,
+            axis: null,
+            reset: true,
+            easing: "cubic-bezier(.03,.98,.52,.99)",
+          }}
+        >
+          <Box
+            display="block"
+            border="1px solid white"
+            borderRadius="2px"
+            overflow={"hidden"}
+            w="100%"
+          >
+            <Image
+              src={project.image}
+              alt={`${project.name} image`}
+              width="500px"
+              height="300px"
+              layout="responsive"
+            />
+          </Box>
+        </Tilt>
+      </Container>
     </Stack>
   );
 };
+
+// react tilt
+// row
 
 export default ProjectItem;
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,12 +4,12 @@ import ProjectItem from "./ProjectItem";
 
 const Projects = () => {
   return (
-    <Box maxW={["90%"]} mx="auto" height={"100%"}>
+    <Box maxW={["90%", "70%"]} mx="auto" height={"100%"}>
       <Text fontSize={"2.2em"} letterSpacing="1px" fontWeight="700">
         Projects
       </Text>
 
-      <Wrap mt="1em" direction={"column"} spacing="5">
+      <Wrap mt="1em" spacing="5">
         {data.projects.map((project) => {
           return (
             <WrapItem key={project.name}>
